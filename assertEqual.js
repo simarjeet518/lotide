3581e5a91a32ad5d2b3d1351acf65c93646a93f2
Simarjeet Kaur <simarjeet518h@gmail.com>
2021-11-23 00:57:38 +0000

    assertion functionality added

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  let msg;
+  if (actual === expected) {
+    msg = `✅✅✅  Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    msg = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  }
+  console.log(msg);
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(5, 1);
+assertEqual("SImar","SImar");
