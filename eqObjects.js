707c576483799d29c8102ce97f282d8658e068c3
Simarjeet Kaur <simarjeet518h@gmail.com>
2021-11-27 08:46:19 +0000

    object recursion added

--- eqObjects.js
+++ eqObjects.js
@@ -22,41 +22,53 @@ const eqArrays = function(arr1 , arr2) {
   return result;
 };
 const eqObjects = function(object1, object2) {
-   let result;
+   let result = false;
    if(Object.keys(object1).length === Object.keys(object2).length){
         let object1Keys = Object.keys(object1);
         //console.log(object1Keys);
         for(let keys of object1Keys){
           //console.log(keys);
-          if(typeof(object1[keys] === typeof(object2[keys]))){
+         // console.log(typeof(object2[keys]));
+          if(typeof(object1[keys]) === typeof(object2[keys])){
              if(Array.isArray(object2[keys])){
-             // console.log("here");
+              
+           //  console.log("here");
               result = (eqArrays(object1[keys],object2[keys]));
+              console.log(result);
+              //return result;
               if(!result){
                 break;
               }
-            } else if(object1[keys] === object2[keys]){
-             // console.log(typeof(object1[keys]));
+            } else if(typeof(object1[keys]) === "number" && typeof(object2[keys]) === "number"){
+              //console.log((object1['a']));
               result =true;
-            } else {  
-              //console.log("line 37");
-              result = false;
-              break;
+            } else if(typeof(object1[keys]) === "object" && typeof(object2[keys]) === "object"){  
+             // console.log("line 37");
+                 result = eqObjects(object1[keys],object2[keys]);   //if result is false stops evaluate 
+                 if(!result){
+                   break;
+                 }
+            //  result = false;
+           //   break;
             }
           }  else {
             //console.log("wrong");
             result = false;
-            break;
+            return result;
+            
           }
-        }
+        } 
    }
    return result;
 };
-const cd = { c: "1", d: ["2", 3] };
+const cd = { c: "1", d: ["2", 3] ,e :["2" ,"4"] };
 const dc = { d: ["2", 3], c: "1" };
 
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2),true); 
-assertEqual(eqObjects(cd, dc),true); 
-assertEqual(eqObjects(dc, cd2),true); 
+const cd2 = { c: "1", d: ["2", 3] ,e: [2 ,4]};
+assertEqual(eqObjects(cd, cd2),false); 
+assertEqual(eqObjects(cd, dc),false); 
+assertEqual(eqObjects(dc, cd2),false); 
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }) ,true);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }),false);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }),false);
