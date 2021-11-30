3eefb487a148fe6c4d34e9da91d390f5db98a016
Simarjeet Kaur <simarjeet518h@gmail.com>
2021-11-30 00:33:50 +0000

    index files completed

--- index.js
+++ index.js
@@ -17,22 +17,5 @@ const takeUntil = require('./takeUntil');
 const without = require('./without');
 
 
-module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  letterPositions: letterPositions,
-  map: map,
-  takeUntil: takeUntil,
-  without: without,
-  assertArrayEqual: assertArrayEqual,
-  assertEqual: assertEqual,
-  assertObjectEqual: assertObjectEqual
+module.exports = {head, tail, middle,countLetters,countOnly,eqArrays, eqObjects,findKey,findKeyByValue,flatten,letterPositions,map,takeUntil,without,assertArrayEqual,assertEqual,assertObjectEqual
 };
